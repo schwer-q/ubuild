b9823e569e1ba51ac1dd3a3b3465115e9ee77dbb
schwer_q <schwer_q@outlook.com>
2013-12-28 11:54:41 +0100

    Add make.mk

---
+++ make-rules/make.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (c) 2013, Quentin Schwerkolt
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+#   list of conditions and the following disclaimer in the documentation and/or
+#   other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+MAKE_BUILD=		$(PROGRESSDIR)/built
+MAKE_INSTALL=		$(PROGRESSDIR)/installed
+
+COMPONENT_INSTALL_ARGS+=	DESTDIR=$(INSTALLDIR)
+COMPONENT_INSTALL_TARGETS+=	install
+
+$(PROGRESSDIR)/built:		prep
+	$(COMPONENT_BUILD_PRE)
+	(cd $(BUILDDIR); $(ENV) $(COMPONENT_BUILD_ENV) \
+		$(MAKE) $(COMPONENT_BUILD_ARGS) $(COMPONENT_BUILD_TARGETS))
+	$(COMPONENT_BUILD_POST)
+	@$(TOUCH) $@
+
+$(PROGRESSDIR)/installed:	$(PROGRESSDIR)/built
+	$(COMPONENT_INSTALL_PRE)
+	(cd $(BUILDDIR); $(ENV) $(COMPONENT_INSTALL_ENV) \
+		$(MAKE) $(COMPONENT_INSTALL_ARGS) $(COMPONENT_INSTALL_TARGETS))
+	$(COMPONENT_INSTALL_POST)
+	@$(TOUCH) $@
+
+clean::
+	@$(RMDIR) $(BUILDDIR)
